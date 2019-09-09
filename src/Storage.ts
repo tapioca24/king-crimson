121e16d6245afa4f0ef6bb884566a4ec9f596ff5
tapioca24 <tapioca_niconico@yahoo.co.jp>
2019-09-09 22:27:20 +0900

    データの追加処理を追加

--- src/Storage.ts
+++ src/Storage.ts
@@ -1,7 +1,13 @@
 import AVLTree from "avl";
 
-class Storage {
+class Storage<T extends KingCrimson.SequentialData> {
   private tree = new AVLTree();
+
+  insert(items: T[]) {
+    for (const item of items) {
+      this.tree.insert(item.timestamp, item);
+    }
+  }
 }
 
 export default Storage;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import Storage from "@/Storage";
 import Fetched from "@/Fetched";
 import Fetcher, { FetchFn, UpdatePayload } from "@/Fetcher";
 
 class KingCrimson<T extends KingCrimson.SequentialData> {
+  private storage = new Storage<T>();
   private fetched = new Fetched();
   private fetcher: Fetcher<T> | null = null;
 
@@ -16,7 +18,9 @@ class KingCrimson<T extends KingCrimson.SequentialData> {
 
     this.fetcher.on("update", (payload: UpdatePayload<T>) => {
       // save data to storage
+      this.storage.insert(payload.items)
       // save period to fetched
+      this.fetched.insert(payload.period)
     });
   }
 
