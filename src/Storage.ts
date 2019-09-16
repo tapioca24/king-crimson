531a9d5df69332bca715d39ba93b45ec45477c12
tapioca24 <tapioca_niconico@yahoo.co.jp>
2019-09-16 14:04:47 +0900

    Storage クラスに search メソッドを実装

--- src/Storage.ts
+++ src/Storage.ts
@@ -1,12 +1,33 @@
 import AVLTree from "avl";
+import { Interval } from "luxon";
 
 class Storage<T extends KingCrimson.SequentialData> {
   private tree = new AVLTree();
+
+  get size() {
+    return this.tree.size;
+  }
 
   insert(items: T[]) {
     for (const item of items) {
       this.tree.insert(item.timestamp, item);
     }
+  }
+
+  /**
+   * 指定範囲に含まれるデータを検索する
+   * @param period 指定範囲
+   */
+  search(period: Interval) {
+    const start = period.start.toMillis();
+    const end = period.end.toMillis();
+
+    const list: T[] = [];
+    this.tree.range(start, end, node => {
+      const data = node.data as T;
+      list.push(data);
+    });
+    return list;
   }
 }
 
