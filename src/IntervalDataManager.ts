0d7f45e1e5bf078d87ff13f32bd028a9f4d575bb
tapioca24 <tapioca_niconico@yahoo.co.jp>
2019-10-02 20:25:53 +0900

    IDM クラスに search メソッドを実装

--- src/IntervalDataManager.ts
+++ src/IntervalDataManager.ts
@@ -2,9 +2,7 @@ import { DateTime, Duration, Interval } from "luxon";
 import { FetchFn, UpdatePayload } from "@/Fetcher";
 import KingCrimson, { KingCrimsonOptions } from "./KingCrimson";
 
-interface IntervalDataManagerOptions extends KingCrimsonOptions {
-  dataDurationLimit?: Duration;
-}
+interface IntervalDataManagerOptions extends KingCrimsonOptions {}
 type IntervalDataManagerConfig = Required<
   Omit<IntervalDataManagerOptions, keyof KingCrimsonOptions>
 >;
@@ -12,11 +10,12 @@ type IntervalDataManagerConfig = Required<
 class IntervalDataManager<
   T extends KingCrimson.IntervalData
 > extends KingCrimson<T> {
+  // 取得したデータで最も大きい duration を保持しておくための変数
+  private maxDuration: Duration = Duration.fromObject({ millisecond: 0 });
+
   constructor(fetchFn: FetchFn<T>, options: IntervalDataManagerOptions = {}) {
     super(fetchFn, options);
-    const defautls: IntervalDataManagerConfig = {
-      dataDurationLimit: Duration.fromObject({ minute: 1 })
-    };
+    const defautls: IntervalDataManagerConfig = {};
     this.config = Object.assign({}, defautls, this.config);
   }
 
@@ -37,12 +36,39 @@ class IntervalDataManager<
         period = Interval.fromDateTimes(period.start, end);
       }
 
+      // maxDuration を更新する
+      for (const item of items) {
+        if (item.duration > this.maxDuration.valueOf()) {
+          this.maxDuration = Duration.fromMillis(item.duration);
+        }
+      }
+
+      // データを追加する
       this.storage.insert(items.filter(item => item.duration > 0));
       this.fetched.insert(period);
     });
   }
 
-  search(datetime: DateTime) {}
+  search(datetime: DateTime) {
+    if (this.storage.size === 0) {
+      return null;
+    }
+
+    // 候補のデータを絞り込む
+    const period = Interval.before(datetime, this.maxDuration);
+    const items = this.storage.search(period);
+
+    // 得られた候補から `datetime` が含まれるデータを探索する
+    const item = items.find(item => {
+      const start = DateTime.fromMillis(item.timestamp);
+      const duration = Duration.fromMillis(item.duration);
+      const period = Interval.after(start, duration);
+      return period.contains(datetime);
+    });
+
+    // 見つかったデータを返す（見つからなければ `null` を返す）
+    return item || null;
+  }
 }
 
 export default IntervalDataManager;
